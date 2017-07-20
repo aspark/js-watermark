b14c5fe0af8774e7e3d0ba375047e9965b5ec162
aspark <tsui.jacky@gmail.com>
2017-07-20 17:57:44 +0800

    tile mode

--- src/wm.js
+++ src/wm.js
@@ -36,10 +36,34 @@
         return str.replace(/(\s+$)|(^\s+)/ig, '');
     }
 
+    function getRotationDegrees(obj) {
+        var matrix = obj.css("-webkit-transform") ||
+        obj.css("-moz-transform")    ||
+        obj.css("-ms-transform")     ||
+        obj.css("-o-transform")      ||
+        obj.css("transform");
+        if(matrix !== 'none') {
+            var values = matrix.split('(')[1].split(')')[0].split(',');
+            var a = values[0];
+            var b = values[1];
+            var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+        } else {
+            var angle = 0;
+        }
+
+        return angle;//(angle < 0) ? angle + 360 : angle;
+    }
+
+    function getElements(selector){
+        return document.querySelectorAll(selector);
+    }
+
     var wmHTML = function(cfg){//only for ie 8
-        this.attach = function(){
+        this.textClassName = 'wm-text-placeholder';
+
+        this.addTextStyle = function(){
             //add style for span
-            var style = ".wm-span-placeholder{\
+            var style = "."+this.textClassName+"{\
                     opacity:"+cfg.opactiy+";\
                     -webkit-opacity:"+cfg.opactiy+";\
                     -moz-opacity:"+cfg.opactiy+";\
@@ -57,12 +81,16 @@
                 }\
             }";
             addStyle(style);
+        }
+
+        this.attach = function(){
+            this.addTextStyle();
 
             //create span for watermark
-            var elements = document.querySelectorAll(cfg.selector);
+            var elements = getElements(cfg.selector);
             for(var i=0; i<elements.length; i++){
                 var span = document.createElement("span");
-                span.className = 'wm-span-placeholder';
+                span.className = this.textClassName;
                 span.style.filter = "alpha(opacity="+cfg.opactiy*100+")";
                 span.innerHTML = cfg.content;
                 elements[i].appendChild(span);
@@ -75,7 +103,7 @@
                 return;
 
             isHidden = false;
-            var elements = document.querySelectorAll('.wm-span-placeholder');
+            var elements = getElements('.'+this.textClassName);
             for(var i = 0; i<elements.length; i++){
                 elements[i].innerHTML=cfg.content;
             }
@@ -86,7 +114,7 @@
                 return;
 
             isHidden = true;
-            var elements = document.querySelectorAll('.wm-span-placeholder');
+            var elements = getElements('.'+this.textClassName);
             for(var i = 0; i<elements.length; i++){
                 elements[i].innerHTML='';
             }
@@ -162,6 +190,59 @@
         }
     }
 
+    var wmTile = function(cfg){
+        wmHTML.apply(this, arguments);
+
+        var defaultCfg= {
+            row: 6,
+            rowInterval: 90,
+            col: 3,
+            colInterval: 180,
+            unit: 'px'
+        };
+
+        cfg = merge(defaultCfg, cfg);
+
+        this.containerClassName="wm-container";
+        this.addContainerStyle = function(){
+            var style = "."+this.containerClassName+"{\
+                position:relative;\
+                top:0;\
+                z-index:999;\
+                overflow:hidden;\
+            }";
+            addStyle(style);
+        }
+
+        this.createContainer = function(){
+            var container = document.createElement('div');
+            container.className=this.containerClassName;
+
+            for (var r = 0; r < cfg.row; r++) {
+                for (var c = 0; c < cfg.col; c++) {
+                    var tile = document.createElement('span');
+                    tile.className=this.textClassName;
+                    tile.style.marginTop=(r * cfg.rowInterval) + cfg.unit;
+                    tile.style.marginLeft=(c * cfg.colInterval) + cfg.unit;
+                    tile.innerHTML = cfg.content;
+                    container.append(tile);
+                }
+            }
+
+            return container;
+        }
+
+        this.attach = function(){
+            // this.addContainerStyle();
+            this.addTextStyle();
+
+            var elements = getElements(cfg.selector);
+            for(var i=0;i<elements.length;i++){
+                elements[i].appendChild(this.createContainer());
+            }
+        }
+    }
+
     function hackCopy(runtime){
         addListener("copy", function(){
             runtime.hide();
@@ -179,6 +260,8 @@
             "fontsize":21,
             "rotate":-30,
             "suffix":"::before",
+            "mode":null,//"css", "html"
+            "tile":false,
             "content":""
         };
 
@@ -189,9 +272,19 @@
             merge(cfg, options);
         }
 
-        var runtime = ('\v'=='v')?new wmHTML(cfg):new wmCSS3(cfg);//use html for ie8
+        var runtime = null;
+        if(cfg.tile)
+            runtime = new wmTile(cfg);
+        else{
+            if(cfg.mode =='css')
+                runtime = new wmCSS3(cfg);
+            else if(cfg.mode == 'html')
+                runtime = new wmHTML(cfg);
+            else
+                runtime = ('\v'=='v')?new wmHTML(cfg):new wmCSS3(cfg);//use html for ie8
+        }
 
-        if(isIE || runtime instanceof wmHTML){
+        if(isIE || !(runtime instanceof wmCSS3)){
             hackCopy(runtime);
         }
 
